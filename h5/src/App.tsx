f1c3ef14ed3e87c7f4a4b482c28009e3fe1f291a
dongguangze <dongguangze@bytedance.com>
2025-04-03 14:07:26 +0800

    fix: h5 build

--- h5/src/App.tsx
+++ h5/src/App.tsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import NoteEdit from './pages/NoteEdit';
